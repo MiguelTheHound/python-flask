360fbaadfac289215597d73f1ecac9c6239a6d6f
MiguelTheHound <miguelvladimir79@gmail.com>
2025-07-17 00:14:50 +0100

    Construção e modelagem do banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 #Importação das dependência necessárias
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY__DATABASE__URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+#Modelagem do banco de dados(id,nome,preço,descrição)
+
 
 # Definição das rotas e funções
 
