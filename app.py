0c4e12a4088c26b58fe03e1ea477bbddfb9fef1d
MiguelTheHound <miguelvladimir79@gmail.com>
2025-07-17 00:58:09 +0100

    Criação do banco de dados

--- app.py
+++ app.py
@@ -5,20 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY__DATABASE__URI'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 
 #Modelagem do banco de dados(id,nome,preço,descrição)
-
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    descricao = db.Column(db.Text, nullable=True)
 
 # Definição das rotas e funções
-
-@app.route('/')
-
-def hello_world():
-    return "hello world"
-
-if __name__ == "__main__":
-
-    app.run(debug=True)
