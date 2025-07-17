bcf698262c13c4c2a5b0987b929df185d7a94450
MiguelTheHound <miguelvladimir79@gmail.com>
2025-07-17 13:54:15 +0100

    Adicionei as rotas para add e delete os produtos, um pequeno problema no postman, erro 404, em vias de ser resolvido

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Importação das dependência necessárias
-from flask import Flask , request
+from flask import Flask , request, jsonify
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,6 +20,23 @@ class Product(db.Model):
 
 @app.route('/api/products/add' , methods=["POST"])
 
-def add_product():
-    dados = request.json
-    return dados
+@app.run(debug=True)
+
+def add_produto():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"],price=data["price"],description=data.get("description", "Descrição não encontrada"))
+        db.session.add(Product)
+        db.session.commit()
+        return "Produto cadastrado"
+    return jsonify({"message":"Dados inválidos"}), 400
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(Product)
+        db.session.commit()
+        return "Produto removido"
+    return jsonify({"message":"Dados não encontrados"}), 404
